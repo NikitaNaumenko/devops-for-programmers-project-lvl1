c69f83e97a29dfa50a8a6c94ddb3c46b4c9769d6
Nikita Naumenko <naumenkoniki@gmail.com>
2021-05-04 17:35:10 +0300

    finish step 7

--- config/environments/development.js
+++ config/environments/development.js
@@ -10,10 +10,10 @@ export default async (app) => {
   app.config.cacheModules = false;
 
   app.config.db = {
-    type: process.ENV.NODOS_DB_TYPE,
-    database: process.ENV.NODOS_DB_NAME,
-    host: process.ENV.NODOS_DB_HOSTNAME,
-    username: process.ENV.NODOS_DB_USERNAME,
-    password: process.ENV.NODOS_DB_PASSWORD,
+    type: process.env.NODOS_DB_TYPE,
+    database: process.env.NODOS_DB_NAME,
+    host: process.env.NODOS_DB_HOSTNAME,
+    username: process.env.NODOS_DB_USERNAME,
+    password: process.env.NODOS_DB_PASSWORD,
   };
 };
